1f8f14db65595ad40d0a923872984f4ca0cf8376
Danilo <danilo.fd@live.com>
2024-07-09 16:06:25 -0400

    Created console project where the user can guess the number from a specific number range.

---
+++ HighOrLow/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program {
+    static void Main(string[] args){
+
+        int targetNumber;
+        int guessNumber;
+        int roundCount = 0;
+        string guessString;
+        
+        Random rnd = new Random();
+        targetNumber = rnd.Next(1, 100);
+
+        do {
+            roundCount++;
+            Console.Write("Please enter a guess between 1 and 100: ");
+            guessString = Console.ReadLine();
+
+            guessNumber = int.Parse(guessString);
+        
+            if(guessNumber == targetNumber){
+                Console.WriteLine("Nice job!");
+            }else if(guessNumber > targetNumber){
+                Console.WriteLine("Too high!");
+            }else {
+                Console.WriteLine("Too low!");
+            }
+
+        } while (guessNumber != targetNumber);
+        Console.WriteLine($"Finish with {roundCount} attempts!");
+
+    }
+}
